33b2feb67c0b2e5f2fd2582a2daae865fd8b1ffd
Howhowchen <debby8698@gmail.com>
2022-09-13 01:19:41 +0800

    feat: add app.js init

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const routes = require('./routes')
+require('./config/mongoose')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extnane: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.use(express.static('public'))
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use(routes)
+
+app.listen(port, () => {
+  console.log(`The server is listening on http://localhost:${port}`)
+})
