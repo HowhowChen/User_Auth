ec84f2a4cb18423d35422a37e5ebd82c61b13187
Howhowchen <debby8698@gmail.com>
2022-09-13 20:36:24 +0800

    refactor: modify index.hbs

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const home = require('./modules/home')
 const user = require('./modules/user')
 
+router.use('/', home)
 router.use('/user', user)
 
 module.exports = router
